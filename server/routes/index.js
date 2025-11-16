edd20ecc1e22148a600778e0e4eea0a1d702fc44
PiratheeshG <piratheesh.gunaseelan@ontariotechu.net>
2025-11-16 00:38:24 -0500

    Cleaning up layout of website

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,23 +14,6 @@ router.get('/home', function(req, res, next) {
     title: 'Caltrack'
   });
 });
-/* GET About Me page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { 
-    title: 'About Me'
-  });
-});
-/* GET Projects page. */
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { 
-    title: 'Projects'
-  });
-});
-/* GET Contact Me. */
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { 
-    title: 'Contact Me'
-  });
-});
+
 
 module.exports = router;
